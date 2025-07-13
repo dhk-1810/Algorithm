d673dddeaa89eee7537463e0e7938ee4caccffe0
김도현 Dohyeon Kim <henly5@naver.com>
2025-07-13 14:55:05 +0900

    [Gold V] Title: 수 고르기, Time: 20 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.cc
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -1,4 +1,4 @@
-// 2230 - 이분탐색
+// 2230 - 투 포인터
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -12,12 +12,13 @@ int main() {
     for (int i = 0; i < n; i++) cin >> a[i];
     sort(a, a+n);
     
-    int ans = 2100000000;
-    for (int i = 0; i < n; i++) {
-        int target = a[i] + m;
-        int idx = lower_bound(a, a + n, target) - a;
-        if (idx < n) ans = min(ans, a[idx] - a[i]);
+    int mn = 2147483647;
+    int en = 0;
+    for (int st = 0; st < n; st++) {
+        while (en < n && a[en] - a[st] < m) en++; // 차이가 m보다 작을때 en 이동.
+        if (en == n) break;
+        mn = min(mn, a[en] - a[st]);
     }
         
-    cout << ans << '\n';
+    cout << mn << '\n';
 }
