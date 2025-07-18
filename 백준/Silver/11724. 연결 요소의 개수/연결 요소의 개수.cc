06f04592dfc1edc2d78f22d326f7ba9e74e46bb8
김도현 Dohyeon Kim <henly5@naver.com>
2025-07-18 12:19:33 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 76 ms, Memory: 6536 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,40 @@
+// 11724
+// 연결 요소 = 바다 위 섬의 개수
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj[1005]; // 벡터를 원소로하는 배열
+bool vis[1005];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, m;
+    cin >> n >> m;
+    
+    while (m--){
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    int num = 0;
+    for (int i = 1; i <= n; i++){ // 각 정점에 대해 BFS 돌림.
+        if (vis[i]) continue;
+        
+        num++;
+        queue<int> q;
+        q.push(i);
+        vis[i] = true;
+        while (!q.empty()) {
+            int cur = q.front();
+            q.pop();
+            for (auto nxt : adj[cur]){
+                if (vis[nxt]) continue;
+                q.push(nxt);
+                vis[nxt] = true;
+            }
+        }
+    }
+    cout << num << '\n';
+}
