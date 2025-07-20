0a090133ead07a74ec8341fed2eb115605d1c831
김도현 Dohyeon Kim <henly5@naver.com>
2025-07-20 22:17:50 +0900

    [Gold III] Title: 줄 세우기, Time: 32 ms, Memory: 3956 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,34 @@
+// 1991
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> adj[32005];
+int deg[32005];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    
+    while (m--) {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        deg[b]++;
+    }
+    
+    queue<int> q;
+    for (int i = 1; i <= n; i++)
+        if (deg[i] == 0) q.push(i);
+    
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        cout << cur << ' ';
+        for (int nxt : adj[cur]){
+            deg[nxt]--;
+            if (deg[nxt] == 0) q.push(nxt);
+        }
+    }
+}
